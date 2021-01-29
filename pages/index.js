7a6ccfa724c29c9768b5ea812fe887236579ccfd
15483114 <15483114.ik@usp.br>
2021-01-29 19:59:27 -0300

    Término da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
-import styled from 'styled-components'
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
+import React from 'react';
+import styled from 'styled-components';
 import Head from 'next/head';
+
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-const BackgroundImage = styled.div`
-background-image: url(${db.bg});
-flex: 1;
-background-size: cover;
-background-position: center;
-
-`;
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
@@ -24,35 +28,52 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
-  return(
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  return (
     <QuizBackground backgroundImage={db.bg}>
-      <Head> 
+      <Head>
         <title> Quiz das Fontes</title>
-
       </Head>
-    <QuizContainer>
-    <QuizLogo />
-    <Widget>
-    <Widget.Header>
-        <h1>Quiz das fontes</h1>
-        </Widget.Header>
-    <Widget.Content>
-      Já passou horas escolhendo a fonte certa pro seu PowerPoint? Então esse quiz é pra você.
-    </Widget.Content>
-    </Widget>
-    <Widget>
-            <Widget.Content>
-              <h1>Quizes da Galera</h1>
-
-              <p>lorem ipsum dolor sit amet...</p>
-            </Widget.Content>
-          </Widget>
-          <Footer />
-        </QuizContainer>
-        <GitHubCorner projectUrl="https://github.com/15483114" />
-      </QuizBackground>
-    
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz das fontes</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+             
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submisssão por meio do react');
+              // router manda para a próxima página
+            }}
+            >
+              <input
+                onChange={function(infosDoEvento){
+                  //name = infosDoEvento.target.value;
+                setName(infosDoEvento.target.value);
+                }}
+                placeholder="Qual o seu nome?" />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar 
+                {' '}
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/15483114" />
+    </QuizBackground>
   );
 }
